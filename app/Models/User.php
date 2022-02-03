7e1d2ea1b38473a2da6813955aa02cc66e767942
taoqun <taoqun8316@163.com>
2022-02-04 02:33:32 +0800

    no message

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use DefaultDatetimeFormat;
 
     protected $fillable = [
         'name',
