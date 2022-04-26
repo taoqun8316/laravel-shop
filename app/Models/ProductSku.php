a6f78d6973eef13e17d0093d1d089376ed25a869
taoqun <taoqun8316@163.com>
2022-04-27 00:11:25 +0800

    add

---
+++ app/Models/ProductSku.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+
+class ProductSku extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+}
