be1a522d13b2d02cf60c256e7914e0fe3b327faf
taoqun <taoqun8316@163.com>
2022-04-29 00:32:33 +0800

    add

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,12 +6,20 @@ use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 use App\Models\ProductSku;
 use Illuminate\Http\Request;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
     public function index(Request $request)
     {
-        $items = $request->user()->cartItems()->with(["productSku.product"])->get();
+        $items = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy("last_used_at", "desc")->get();
 
         return $this->success("获取成功", [
@@ -22,29 +30,17 @@ class CartController extends Controller
 
     public function add(AddCartRequest $request)
     {
-        $user   = $request->user();
         $skuId  = $request->input('sku_id');
         $amount = $request->input('amount');
 
-        if ($cart = $user->cartItems()->where("product_sku_id", $skuId)->first()){
-            $cart->update([
-                "amout" => $cart->amount+$amount
-            ]);
-        }else{
-            $cartItem = new CartItem([
-                "amount" => $amount
-            ]);
-            $cartItem->user()->associate($user);
-            $cartItem->productSku()->associate($skuId);
-            $cartItem->save();
-        }
+        $this->cartService->add($skuId, $amount);
 
         return $this->success("添加成功",[]);
     }
 
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where("product_sku_id", $sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return $this->success("移除成功",[]);
     }
 
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,8 +8,10 @@ use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
+use App\Services\OrderService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Services\CartService;
 
 class OrdersController extends Controller
 {
@@ -33,59 +35,12 @@ class OrdersController extends Controller
         ]);
     }
 
-    public function store(OrderRequest $request)
+    public function store(OrderRequest $request, OrderService $orderService)
     {
         $user = $request->user();
-        $order = \DB::transaction(function () use ($user, $request){
-            $address = UserAddress::find($request->input("address_id"));
-            $address->update([
-                "last_used_at" => Carbon::now()
-            ]);
+        $address = UserAddress::find($request->input("address_id"));
+        $order = $orderService->store($user, $address, $request->input("remark"),$request->input("items"));
 
-            $order = new Order([
-                'address' => [ // 将地址信息放入订单中
-                    'address' => $address->full_address,
-                    'zip' => $address->zip,
-                    'contact_name' => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
-                ],
-                'remark' => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-            $order->user()->associate($user);
-            $order->save();
-
-            $totalAmount = 0;
-            $items = $request->input("items");
-            foreach ($items as $data){
-                $sku  = ProductSku::find($data['sku_id']);
-                $item = $order->items()->make([
-                    "amount"=>$data['amount'],
-                    "price" => $sku->price
-                ]);
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku->id);
-                $item->save();
-                $totalAmount += $data['amount']*$sku->price;
-
-                //减少库存
-                if ($sku->decreaseStock($data['amount'])<=0){
-                    throw new ApiException("库存不足");
-                }
-            }
-
-            $order->update([
-                "total_amount" => $totalAmount
-            ]);
-
-            $skuIds = collect($items)->pluck("sku_id");
-            $user->cartItems()->whereIn("product_sku_id", $skuIds)->delete();
-
-            return $order;
-        });
-
-        //(new CloseOrder($order, config('app.order_ttl')))->handle();die;
-        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
         return $this->success("添加成功", [
             "order" => $order
         ]);
